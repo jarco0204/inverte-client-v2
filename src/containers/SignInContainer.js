0f976a7d17bd51c96d2a4097b556d6fedb839086
atellezcastr <atellezcastr@mun.ca>
2022-05-21 20:23:20 -0230

    feat: working auth

--- src/containers/SignInContainer.js
+++ src/containers/SignInContainer.js
@@ -1,57 +1,58 @@
-import React, { useState } from 'react'
-import SignIn from '../components/SignIn'
-import { Auth } from 'aws-amplify';
+import React, { useState } from "react";
+import SignIn from "../components/SignIn";
+import { Auth } from "aws-amplify";
 
-function SignInContainer ({
-    authorized=console.log,
-    setAuthorized = console.log
+function SignInContainer({
+    authorized = console.log,
+    setAuthorized = console.log,
 }) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [fetching, setFetching] = useState(false)
-    const [error, setError] = useState(undefined)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [fetching, setFetching] = useState(false);
+    const [error, setError] = useState(undefined);
     //const cognito = useCognito() (?)
 
-    const noPermissionError = `Your account ${authorized} doees not have permission to use this app. Try signing in with another account.`
+    const noPermissionError = `Your account ${authorized} doees not have permission to use this app. Try signing in with another account.`;
 
     /* 
     To Do:
     SingIn functionality using AWS amplify
     */
-   async function signing(){
-       try {
-           const user = await Auth.signIn(email, password);
-       } catch(error) {
-           console.log("error signing in", error)
-           setError('Wrong credentials')
-       }
-   }
-   function handleLogIn (event) {
-       console.log(event)
-       if (email ==='') {
-           setError('Enter your email')
-       } else if (password==='') {
-           setError('Enter your password')
-       } else {
-           setError(undefined)
-           setFetching(true)
-           console.log(email)
-           console.log(password)
-           signing()
-       }
-   }
-   return (
-       <SignIn 
-       handleLogIn={handleLogIn}
-       email={email}
-       password={password}
-       error={authorized? noPermissionError : error}
-       fetching={fetching}
-       setEmail={setEmail}
-       setPassword={setPassword}
-    //    RouterLink={RouterLink}
-       />
-   )
+    async function signing() {
+        try {
+            const user = await Auth.signIn(email, password);
+            console.log(user);
+        } catch (error) {
+            console.log("error signing in", error);
+            setError("Wrong credentials");
+        }
+    }
+    function handleLogIn(event) {
+        console.log(event);
+        if (email === "") {
+            setError("Enter your email");
+        } else if (password === "") {
+            setError("Enter your password");
+        } else {
+            setError(undefined);
+            setFetching(true);
+            console.log(email);
+            console.log(password);
+            signing();
+        }
+    }
+    return (
+        <SignIn
+            handleLogIn={handleLogIn}
+            email={email}
+            password={password}
+            error={authorized ? noPermissionError : error}
+            fetching={fetching}
+            setEmail={setEmail}
+            setPassword={setPassword}
+            //    RouterLink={RouterLink}
+        />
+    );
 }
 
 export default SignInContainer;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Amplify from "aws-amplify";
+import config from "./config";
+
+Amplify.configure({
+    Auth: {
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
