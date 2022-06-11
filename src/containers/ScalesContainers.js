cfb002f686d3ad10dd7c69eb43c1a215bdaf7482
Johan Arcos (El Puma) <jarco0204@outlook.com>
2022-06-11 13:34:10 -0230

    added aws real-time connection

--- src/containers/ScalesContainers.js
+++ src/containers/ScalesContainers.js
@@ -1,4 +1,33 @@
 import Scale from "../components/Scale";
+// Aws Imports
+import Amplify from "aws-amplify";
+import { AWSIoTProvider } from "@aws-amplify/pubsub/lib/Providers";
+
+Amplify.configure({
+    Auth: {
+        identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+        region: process.env.REACT_APP_REGION,
+        userPoolId: process.env.REACT_APP_USER_POOL_ID,
+        userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    },
+});
+Amplify.addPluggable(
+    new AWSIoTProvider({
+        aws_pubsub_region: process.env.REACT_APP_REGION,
+        aws_pubsub_endpoint: `wss://${String(
+            process.env.REACT_APP_MQTT_ID,
+        )}.iot.${String(process.env.REACT_APP_REGION)}.amazonaws.com/mqtt`,
+    }),
+);
+Amplify.PubSub.subscribe("ESM/1/ESM-1-F0-7/Pub").subscribe({
+    next: (data) => console.log("Message received", data),
+    error: (error) => console.error(error),
+    close: () => console.log("Done"),
+});
+
+/*
+    Main Container Function
+  */
 function ScalesContainer() {
     return (
         <div
