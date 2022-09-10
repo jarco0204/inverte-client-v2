4652c34362a25f03d828e9da6da9f9b68c9eb8f6
Johan Arcos (El Puma) <jarco0204@outlook.com>
2022-09-10 13:43:03 -0230

    Added Web Socket with Amplify

--- src/containers/RealTimeContainer.js
+++ src/containers/RealTimeContainer.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+
+import { PubSub } from "aws-amplify";
+
 import { Line, LineChart, XAxis, YAxis, Tooltip } from "recharts";
 
 /*
@@ -12,12 +15,22 @@ function RealTimeContainer(props) {
 
     const [data, setData] = useState([]);
 
-    // 1. listen for a cpu event and update the state
-    // useEffect(() => {
-    //     socket.on("cpu", (cpuPercent) => {
-    //         setData((currentData) => [...currentData, cpuPercent]);
-    //     });
-    // }, []);
+    // Do you need this inside UseEffect?
+    /*
+        This is format to send data from cloud to app
+        {
+    "timestamp": 6,
+    "inventoryWeight": 20
+        }
+    */
+    PubSub.subscribe("johan/1/P0$8").subscribe({
+        next: (dataCloud) => {
+            console.log("Message received", dataCloud);
+            setData((data) => [...data, dataCloud.value]);
+        },
+        error: (error) => console.error(error),
+        complete: () => console.log("Done"),
+    });
 
     return (
         <div>
@@ -25,9 +38,9 @@ function RealTimeContainer(props) {
                 Real Time Inventory Usage
             </h1>
             <LineChart width={500} height={300} data={data}>
-                <XAxis dataKey="name" />
+                <XAxis dataKey="timestamp" />
                 <YAxis />
-                <Line dataKey="value" />
+                <Line dataKey="inventoryWeight" />
             </LineChart>
         </div>
     );
