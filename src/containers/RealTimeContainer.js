ef70e2a3b42884db9a638787498ab4e3ad8610fe
Johan Arcos (El Puma) <jarco0204@outlook.com>
2022-09-10 20:04:58 -0230

    Finish Real-Time Graph

--- src/containers/RealTimeContainer.js
+++ src/containers/RealTimeContainer.js
@@ -2,8 +2,16 @@ import { useEffect, useState } from "react";
 
 import { PubSub } from "aws-amplify";
 
-import { Line, LineChart, XAxis, YAxis, Tooltip } from "recharts";
+import {
+    Line,
+    LineChart,
+    XAxis,
+    YAxis,
+    ResponsiveContainer,
+    Tooltip,
+} from "recharts";
 
+import CustomToolTipGraph from "../components/CustomToolTipGraph";
 /*
     @Input: Authorized state
     @Output: Graph UI Web Socket
@@ -26,22 +34,51 @@ function RealTimeContainer(props) {
     PubSub.subscribe("johan/1/P0$8").subscribe({
         next: (dataCloud) => {
             console.log("Message received", dataCloud);
-            setData((data) => [...data, dataCloud.value]);
+            // Change Unix Timesetamp to Local Time
+            let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+            d.setUTCMilliseconds(dataCloud.value.timestamp);
+            // console.log(d);
+            let graphEle = {
+                inventoryWeight: dataCloud.value.inventoryWeight,
+                timestamp: d,
+                inventoryName: dataCloud.value.inventoryName,
+            };
+            const updatedDataAr = [...data, graphEle];
+            setData(updatedDataAr);
         },
         error: (error) => console.error(error),
-        complete: () => console.log("Done"),
+        complete: () => console.log("Web Socket Done"),
     });
 
     return (
         <div>
             <h1 style={{ position: "relative", top: "20px", left: "100px" }}>
-                Real Time Inventory Usage
+                Real-Time Inventory Usage
             </h1>
-            <LineChart width={500} height={300} data={data}>
-                <XAxis dataKey="timestamp" />
-                <YAxis />
-                <Line dataKey="inventoryWeight" />
-            </LineChart>
+            <br />
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart data={data}>
+                    <XAxis
+                        dataKey="timestamp"
+                        hasTick
+                        label={{
+                            value: "Time",
+                            angle: 0,
+                        }}
+                    />
+                    <YAxis
+                        label={{
+                            value: "Weight",
+                            angle: 270,
+                            textAnchor: "middle",
+                            dx: -20,
+                        }}
+                        tickMargin={3}
+                    />
+                    <Line dataKey="inventoryWeight" />
+                    <Tooltip content={<CustomToolTipGraph />} />
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     );
 }
