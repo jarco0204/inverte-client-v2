fb191c461909f4c83d63759ff1691f8dfede121b
Johan Arcos (El Puma) <jarco0204@outlook.com>
2023-01-28 20:24:22 -0330

    Warnings Fixed + Scale Card UI

--- src/pages/scales/components/Scale/RadioActions.js
+++ src/pages/scales/components/Scale/RadioActions.js
@@ -4,14 +4,31 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
-export default function RowRadioButtonsGroup({ label1, label2, title }) {
+import MDTypography from "../../../../components/MDTypography";
+
+export default function RadioActions() {
     return (
-        <FormControl>
-            <FormLabel id="demo-row-radio-buttons-group-label">{title}</FormLabel>
-            <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
-                <FormControlLabel value={label1} control={<Radio />} label={label1} />
-                <FormControlLabel value={label2} control={<Radio />} label={label2} />
-            </RadioGroup>
-        </FormControl>
+        <div style={{ display: "flex", gap: "20px", margin: "20px 0", marginLeft: "40px" }}>
+            <FormControl>
+                <MDTypography fontWeight="bold" fontSize="14px">
+                    Unit
+                </MDTypography>
+
+                <RadioGroup row aria-labelledby="demo-radio-buttons-group-label" defaultValue="oz">
+                    <FormControlLabel value="oz" control={<Radio />} label="oz" />
+                    <FormControlLabel value="g" control={<Radio />} label="g" />
+                </RadioGroup>
+            </FormControl>
+            <FormControl>
+                <MDTypography fontSize="14px" fontWeight="bold">
+                    Mode
+                </MDTypography>
+
+                <RadioGroup row aria-labelledby="demo-radio-buttons-group-label" defaultValue="portion">
+                    <FormControlLabel value="portion" control={<Radio />} label="portion" checked />
+                    <FormControlLabel value="normal" control={<Radio />} label="normal" />
+                </RadioGroup>
+            </FormControl>
+        </div>
     );
 }
