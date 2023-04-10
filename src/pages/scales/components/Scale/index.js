c530af5a4c517d711e7e202b69b4d2bf631e6577
Johan Arcos (El Puma) <jarco0204@outlook.com>
2023-04-10 17:36:57 -0230

    Fix: File structure + Add Shadow Manipulation

--- src/pages/scales/components/Scale/index.js
+++ src/pages/scales/components/Scale/index.js
@@ -11,33 +11,26 @@ import Card from "@mui/material/Card";
 import Icon from "@mui/material/Icon";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
 import CardActions from "@mui/material/CardActions";
-
-// import Box from "@mui/material/Box";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-
-// Expand Functionality
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import Collapse from "@mui/material/Collapse";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 // User Components
-import InputAdornments from "./InputAdornments";
-import RadioActions from "./RadioActions";
 import MDBox from "../../../../components/MDBox";
 import MDTypography from "../../../../components/MDTypography";
 import { TareButton, StartButton, ExpandMore } from "./ScaleButtons";
 
-import { IoTDataPlaneClient, UpdateThingShadowCommand } from "@aws-sdk/client-iot-data-plane";
-
 /*
     Main Function Component
 */
-function Scale({ mainScaleData, iotShadowClient }) {
-    /*
-        Core Scale State is fetched from IoT Shadow
-    */
+function Scale({ mainScaleData }) {
+    //  Core Scale State is fetched from IoT Shadow
     console.log("Your channel good sir, ", mainScaleData); // Debug statement
     const [nameIngredient, setNameIngredient] = useState(mainScaleData.state.nameIngredient);
     const [correctWeight, setCorrectWeight] = useState(mainScaleData.state.correctWeight);
@@ -62,69 +55,80 @@ function Scale({ mainScaleData, iotShadowClient }) {
     // }
 
     /*
-        Shadow Client
+        Function to expand scale card when arrow is button is clicked
     */
-    const updateShadow = async (event) => {
-        if (event.target.name === "ingredientNameField") {
-            const getThingShadowRequestInput = {
-                state: {
-                    desired: {
-                        nameIngredient: nameIngredient,
-                    },
-                },
-            };
-            PubSub.publish("$aws/things/P0-08-v2/shadow/update", getThingShadowRequestInput);
-            console.log("update shadow");
-        }
+    const [expanded, setExpanded] = useState(false);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
     };
 
     /*
-        Send updated params to APPROPRIATE scale channel
-        whenever a change is detected in the inputs or focus is removed
-
-        Function takes parameter to set data to update state or control
-
-        When action = 1, the scale tares. 
-        When action = 2, the scale starts. 
-        When action = 3, the scale changes unit of mass. 
-        When action = 4, the scale changes the mode of operation
+        Function to handle the change of unit of mass
     */
-    const sendDataAWS = async (control = false, action = null, value = null) => {
-        let msg, finalTopic;
-        if (control) {
-            msg = {
-                control: action,
-                val: value,
-            };
+    const unitOfMassChange = (event) => {
+        // Unit of mass Change
+        if (unitOfMassCode === "g") {
+            setCorrectWeight((correctWeight / 28.35).toFixed(1));
+            setMinOffset((minOffset / 28.35).toFixed(1));
+            setMaxOffset((maxOffset / 28.35).toFixed(1));
 
-            if (action == 3) {
-                // Unit of mass Change
-                if (unitOfMassCode === "oz") {
-                    setCorrectWeight(Math.round(correctWeight * 28.35));
-                    setMinOffset(Math.round(minOffset * 28.35));
-                    setMaxOffset(Math.round(maxOffset * 28.35));
-                } else {
-                    setCorrectWeight((correctWeight / 28.35).toFixed(1));
-                    setMinOffset((minOffset / 28.35).toFixed(1));
-                    setMaxOffset((maxOffset / 28.35).toFixed(1));
-                }
-            }
-            finalTopic = mainScaleData.topic + "control";
+            setUnitOfMassCode("oz");
         } else {
-            // TODO: Validate/convert the correct type of the parameters scale accepts
-            msg = {
-                msg: "Sending portion parameters from Client to AWS",
-                nameIngredient: nameIngredient,
-                correctWeight: correctWeight,
-                lowerErrorLimit: minOffset,
-                upperErrorLimit: maxOffset,
-            };
-            finalTopic = mainScaleData.topic + "params";
-            console.log(msg);
+            setCorrectWeight(Math.round(correctWeight * 28.35));
+            setMinOffset(Math.round(minOffset * 28.35));
+            setMaxOffset(Math.round(maxOffset * 28.35));
+            setUnitOfMassCode("g");
         }
-        await PubSub.publish(finalTopic, msg);
+
+        updateShadow(event);
     };
 
+    /*
+        Function to update the IoT Device Shadow by using PubSub Amplify MQTT Client
+    */
+    const updateShadow = async (event) => {
+        const getThingShadowRequestInput = { state: { desired: {} } };
+
+        // Determine which property of shadow to update
+        if (event.target.name === "ingredientNameField") {
+            getThingShadowRequestInput.state.desired["nameIngredient"] = nameIngredient;
+        } else if (event.target.name === "correctWeightField") {
+            getThingShadowRequestInput.state.desired["correctWeight"] = correctWeight;
+        } else if (event.target.name === "minOffsetField") {
+            getThingShadowRequestInput.state.desired["lowerErrorLimit"] = minOffset;
+        } else if (event.target.name === "maxOffsetField") {
+            getThingShadowRequestInput.state.desired["upperErrorLimit"] = maxOffset;
+        } else if (event.target.name === "unitOfMassField") {
+            getThingShadowRequestInput.state.desired["unitOfMass"] = unitOfMassCode;
+        } else {
+            console.log("error while trying to change the scale state");
+        }
+
+        // Update Shadow
+        PubSub.publish("$aws/things/P0-08-v2/shadow/update", getThingShadowRequestInput);
+        console.log("Shadow Update..."); // Debug Statement
+    };
+
+    /*
+        Function to send desired action to scale (Tare or Start Guidance)
+        Possible Actions are {1: "tare", 2: "start"}
+    */
+    const sendDataAWS = async (action) => {
+        let msg, finalTopic;
+        msg = {
+            control: action,
+            msg: "Message sent by el PumaV56",
+        };
+        finalTopic = mainScaleData.topic[0] + "control";
+
+        // Perform action on scale
+        PubSub.publish(finalTopic, msg); // Await it not needed
+        console.log("Action Published to Scale..."); // Debug Statement
+    };
+
+    /*
+        Hook to enable real-time communication from scale to client to display IoT Shadows
+    */
     useEffect(() => {
         // Open Web Socket to update data
         PubSub.subscribe("test/1/ts").subscribe({
@@ -148,14 +152,6 @@ function Scale({ mainScaleData, iotShadowClient }) {
         });
     }, []);
 
-    /*
-        Function to expand scale card when arrow is button is clicked
-    */
-    const [expanded, setExpanded] = useState(false);
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
-
     return (
         <Card style={{ width: "300px" }}>
             <MDBox display="flex" justifyContent="space-between" pt={1} px={3}>
@@ -165,19 +161,10 @@ function Scale({ mainScaleData, iotShadowClient }) {
                     </Icon>
                 </MDBox>
 
-                <MDBox textAlign="center" lineHeight={1.2} style={{ margin: "10px 0", paddingRight: "40px" }}>
+                <MDBox lineHeight={1.2} style={{ margin: "10px 0", paddingRight: "40px" }}>
                     {/* <MDTypography fontWeight="bold" color="dark" marginRight={"40px"} fontSize="18px">
                         Scale #P0-08
                     </MDTypography> */}
-                    {/* <InputAdornments
-                        style={{ margin: "10px 0", paddingRight: "40px" }}
-                        valuePlaceholder={nameIngredient}
-                        label={"Ingredient"}
-                        setCorrectWeight={setNameIngredient}
-                        updateShadow={updateShadow}
-                    /> */}
-                    {/* <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-                        <> */}
                     <FormControl sx={{ m: 1, width: 125 }} variant="outlined">
                         <FormHelperText id="outlined-weight-helper-text">Ingredient</FormHelperText>
                         <OutlinedInput
@@ -187,7 +174,6 @@ function Scale({ mainScaleData, iotShadowClient }) {
                                 backgroundColor: "beige",
                             }}
                             value={nameIngredient}
-                            endAdornment={<InputAdornment position="end">{unitOfMassCode}</InputAdornment>}
                             aria-describedby="outlined-weight-helper-text"
                             inputProps={{
                                 "aria-label": "weight",
@@ -196,25 +182,33 @@ function Scale({ mainScaleData, iotShadowClient }) {
                             onBlur={(e) => updateShadow(e)}
                         />
                     </FormControl>
-                    {/* </>
-                    </Box> */}
                 </MDBox>
             </MDBox>
             <div style={{ margin: "auto" }}>
-                <InputAdornments
-                    label={"Correct Portion Weight"}
-                    unitOfMassCode={unitOfMassCode}
-                    valuePlaceholder={correctWeight.toString()}
-                    setCorrectWeight={setCorrectWeight}
-                    updateShadow={updateShadow}
-                    width={"15ch"}
-                />
+                <FormControl sx={{ m: 1, width: 125 }} variant="outlined">
+                    <FormHelperText id="outlined-weight-helper-text">Correct Portion Weight: </FormHelperText>
+                    <OutlinedInput
+                        id="outlined-adornment-weight"
+                        name="correctWeightField"
+                        style={{
+                            backgroundColor: "beige",
+                        }}
+                        value={correctWeight}
+                        endAdornment={<InputAdornment position="end">{unitOfMassCode}</InputAdornment>}
+                        aria-describedby="outlined-weight-helper-text"
+                        inputProps={{
+                            "aria-label": "weight",
+                        }}
+                        onChange={(e) => setCorrectWeight(e.target.value)}
+                        onBlur={(e) => updateShadow(e)}
+                    />
+                </FormControl>
             </div>
             <CardActions disableSpacing>
-                <TareButton name="tare" onClick={() => sendDataAWS(true, 1)}>
+                <TareButton name="tare" onClick={() => sendDataAWS(1)}>
                     Tare
                 </TareButton>
-                <StartButton name="start" onClick={() => sendDataAWS(true, 2)}>
+                <StartButton name="start" onClick={() => sendDataAWS(2)}>
                     {stateButtonStr}
                 </StartButton>
                 <ExpandMore expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
@@ -230,8 +224,42 @@ function Scale({ mainScaleData, iotShadowClient }) {
                     </MDBox>
 
                     <div style={{ display: "flex" }}>
-                        <InputAdornments label={"Under"} unitOfMassCode={unitOfMassCode} valuePlaceholder={minOffset.toString()} setCorrectWeight={setMinOffset} sendDataAWS={sendDataAWS} />
-                        <InputAdornments label={"Over"} unitOfMassCode={unitOfMassCode} valuePlaceholder={maxOffset.toString()} setCorrectWeight={setMaxOffset} sendDataAWS={sendDataAWS} />
+                        <FormControl sx={{ m: 1, width: 125 }} variant="outlined">
+                            <FormHelperText id="outlined-weight-helper-text">Lower Threshold: </FormHelperText>
+                            <OutlinedInput
+                                id="outlined-adornment-weight"
+                                name="minOffsetField"
+                                style={{
+                                    backgroundColor: "beige",
+                                }}
+                                value={minOffset}
+                                endAdornment={<InputAdornment position="end">{unitOfMassCode}</InputAdornment>}
+                                aria-describedby="outlined-weight-helper-text"
+                                inputProps={{
+                                    "aria-label": "weight",
+                                }}
+                                onChange={(e) => setMinOffset(e.target.value)}
+                                onBlur={(e) => updateShadow(e)}
+                            />
+                        </FormControl>
+                        <FormControl sx={{ m: 1, width: 125 }} variant="outlined">
+                            <FormHelperText id="outlined-weight-helper-text">Upper Threshold: </FormHelperText>
+                            <OutlinedInput
+                                id="outlined-adornment-weight"
+                                name="maxOffsetField"
+                                style={{
+                                    backgroundColor: "beige",
+                                }}
+                                value={maxOffset}
+                                endAdornment={<InputAdornment position="end">{unitOfMassCode}</InputAdornment>}
+                                aria-describedby="outlined-weight-helper-text"
+                                inputProps={{
+                                    "aria-label": "weight",
+                                }}
+                                onChange={(e) => setMaxOffset(e.target.value)}
+                                onBlur={(e) => updateShadow(e)}
+                            />
+                        </FormControl>
                     </div>
 
                     <div style={{ margingTop: "10px" }}>
@@ -242,7 +270,14 @@ function Scale({ mainScaleData, iotShadowClient }) {
                         </MDBox>
                     </div>
 
-                    <RadioActions setUnitOfMassCode={setUnitOfMassCode} unitOfMassCode={unitOfMassCode} sendDataAWS={sendDataAWS} />
+                    <div style={{ display: "flex", gap: "20px", margin: "20px 0", marginLeft: "40px" }}>
+                        <FormControl>
+                            <RadioGroup row aria-labelledby="demo-radio-buttons-group-label" name="unitOfMassField" onChange={unitOfMassChange} defaultValue={unitOfMassCode}>
+                                <FormControlLabel value="oz" control={<Radio />} label="oz" />
+                                <FormControlLabel value="g" control={<Radio />} label="g" />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
                 </div>
             </Collapse>
         </Card>
@@ -252,7 +287,6 @@ function Scale({ mainScaleData, iotShadowClient }) {
 // Setting default values for the props of Scale and typechecking props for the Scale
 Scale.propTypes = {
     mainScaleData: PropTypes.object,
-    iotShadowClient: PropTypes.object,
 };
 Scale.defaultProps = {
     mainScaleData: {
