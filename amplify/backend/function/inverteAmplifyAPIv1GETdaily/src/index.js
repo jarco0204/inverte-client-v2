76593231d6a895b1dd0e44465d303a7a18e0e589
Johan Arcos (El Puma) <jarco0204@outlook.com>
2023-05-01 11:37:00 -0230

    add: amplify rules

---
+++ amplify/backend/function/inverteAmplifyAPIv1GETdaily/src/index.js
@@ -0,0 +1,55 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+async function getItem(params) {
+    try {
+        const data = await docClient.get(params).promise();
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
+
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    // console.log(event.queryStringParameters);
+
+    // Params object to retrieve the essential information about restaurant
+    let key = event.queryStringParameters.dayOfYear + "_" + event.queryStringParameters.hourOfDay + "_" + event.queryStringParameters.iotNameThing;
+    console.log(key);
+    const params = {
+        TableName: "inverte_portionEvents_v6",
+        Key: { dayOfYear_hourOfDay_iotNameThing: key, minuteOfHour_secondOfMinute: "60_60" }, // This parameters should come from scale
+    };
+
+    // Call to your Database
+    try {
+        const data = await getItem(params);
+        const restaurant = {
+            message: "Information correctly retrieved from Dynamo using Lambda420",
+            sdkVersion: AWS.VERSION,
+            daily: data.Item,
+        };
+        return {
+            statusCode: 200,
+            //  Uncomment below to enable CORS requests
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify(restaurant),
+        };
+    } catch (err) {
+        return { error: err, statusCode: 404 };
+    }
+};
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,43 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "inverteClientAmplifyAPIV2": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "inverteclientv228c7d778": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "inverteAmplifyAPIv1GETdaily": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "inverteAmplifyAPIv1GETrestaurant": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "inverteAmplifyAPIv1UPDATEmeta": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
