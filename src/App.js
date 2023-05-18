8b79847087ffd2bbcc235ff8e45666e0214f4398
Rohan-16 <rrumjansing@mun.ca>
2023-05-18 15:21:04 -0230

    Fix:Refresh bug

--- src/App.js
+++ src/App.js
@@ -92,8 +92,9 @@ export default function App() {
                 console.log(err);
             }
         }
+
         authSession();
-    }, []);
+    }, [authenticated]);
 
     // Setting the dir attribute for the body element
     useEffect(() => {
