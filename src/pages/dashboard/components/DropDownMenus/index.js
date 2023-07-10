328fd193242e2bb275d2bcd8fdab19dfcb9a3a52
jarco0204 <jaarcos@mun.ca>
2023-07-10 12:25:27 -0230

    Fix: Options Menu Start

--- src/pages/dashboard/components/DropDownMenus/index.js
+++ src/pages/dashboard/components/DropDownMenus/index.js
@@ -1,109 +1,115 @@
 import { styled, alpha } from "@mui/material";
 import { Menu } from "@mui/material";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import MDBox from "../../../../components/MDBox";
-import MDTypography from "../../../../components/MDTypography";
+// import MDTypography from "../../../../components/MDTypography";
 import { useState } from "react";
 import { KeyboardArrowDown } from "@material-ui/icons";
-import {MenuItem} from "@mui/material";
-import { useTheme } from "@mui/material";
+import { MenuItem, ListItemIcon } from "@mui/material";
+// import { useTheme } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
-const StyledMenu = styled(( props ) => (
+const StyledMenu = styled((props) => (
     <Menu
-    elevation={0}
-    color="info"
-    display="inline-block"
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}        
+        elevation={0}
+        color="info"
+        display="inline-block"
+        anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+        }}
+        transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+        }}
+        {...props}
     />
 ))(({ theme }) => ({
-    '& .MuiPaper-root': {
+    "& .MuiPaper-root": {
         borderRadius: 6,
         marginTop: theme.spacing(1),
         minWidth: 180,
-        color:
-        theme.palette.info.main,
-        '& .MuiMenuItem-root': {
-        '& .MuiSvgIcon-root': {
-            fontSize: 18,
-            color: theme.palette.info.main,
-            marginRight: theme.spacing(1.5),
-        },
-        '&:active': {
-            backgroundColor: alpha(
-                theme.palette.info.main,
-                theme.palette.info.main,
-            ),
-        },
+        color: theme.palette.info.main,
+        "& .MuiMenuItem-root": {
+            "& .MuiSvgIcon-root": {
+                fontSize: 18,
+                color: theme.palette.info.main,
+                marginRight: theme.spacing(1.5),
+            },
+            "&:active": {
+                backgroundColor: alpha(theme.palette.info.main, theme.palette.info.main),
+            },
         },
     },
-    }));
+}));
 
-function DropDownMenus({
-    options,
-    selectedIndexRef,
-}) {
-    const [anchorEl, setAnchorEl] = useState(null)
-    const open = Boolean(anchorEl)
+const DropDownMenus = ({ options, selectedIndexRef, selectedIndex, handleClickListItem, handleMenuItemClick }) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
     const handleClose = () => {
         setAnchorEl(null);
     };
-    return(
+    return (
         <MDBox py={1}>
             <MDBox
-            variant="gradient"
-            bgColor="info"
-            color="light"
-            coloredShadow="info"
-            borderRadius="xl"
-            width="10%"
-            height="3rem"
-            justifyContent="center"
-            alignItems="center"
-            display="flex"
-            id="lock-menu"
-            aria-controls={open ? 'lock-menu' : undefined}
-            aria-haspopup="listbox"
-            aria-expanded={open ? 'true' : undefined}
-            disableElevation
-            onClick={handleClick}
-            endIcon={<KeyboardArrowDown />}
-            style={{fontFamily: "Roboto"}}
+                variant="gradient"
+                bgColor="info"
+                color="light"
+                coloredShadow="info"
+                borderRadius="xl"
+                width="10%"
+                height="3rem"
+                justifyContent="center"
+                alignItems="center"
+                display="flex"
+                id="lock-menu"
+                aria-controls={open ? "lock-menu" : undefined}
+                aria-haspopup="listbox"
+                aria-expanded={open ? "true" : undefined}
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDown />}
+                style={{ fontFamily: "Roboto" }}
             >
-                Options
+                <List component="nav" aria-label="Device settings">
+                    <ListItem
+                        id="lock-button"
+                        aria-haspopup="listbox"
+                        aria-controls="lock-menu"
+                        aria-label="when device is locked"
+                        aria-expanded={open ? "true" : undefined}
+                        onClick={handleClickListItem}
+                        style={{ fontFamily: "Roboto" }}
+                    >
+                        <ListItemText secondary={selectedIndex === -1 ? "Ingredient" : options[selectedIndex]} />
+                    </ListItem>
+                </List>
                 <KeyboardArrowDown />
             </MDBox>
             <StyledMenu
-            id="lock-menu"
-            MenuListProps={{
-                'aria-labelledby': 'lock-menu',
-                role: "listbox",
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
+                id="lock-menu"
+                MenuListProps={{
+                    "aria-labelledby": "lock-menu",
+                    role: "listbox",
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
             >
-            {options.map((option, index) => (
-                <MenuItem 
-                key={option} 
-                selected={index === selectedIndexRef.curent} 
-                onClick={handleClose} disableRipple>
-                {option}
-            </MenuItem>
-            ))}
+                {options.map((option, index) => (
+                    <MenuItem key={option} selected={index === selectedIndexRef.curent} onClick={(event) => handleMenuItemClick(event, index)} disableRipple>
+                        {option}
+                    </MenuItem>
+                ))}
             </StyledMenu>
         </MDBox>
-    )
-}
+    );
+};
 
 export default DropDownMenus;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -8,11 +8,11 @@ import PanToolIcon from "@mui/icons-material/PanTool";
 import PrecisionManufacturingRoundedIcon from "@mui/icons-material/PrecisionManufacturingRounded";
 import ScaleRoundedIcon from "@mui/icons-material/ScaleRounded";
 import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemText from "@mui/material/ListItemText";
+// import MenuItem from "@mui/material/MenuItem";
+// import Menu from "@mui/material/Menu";
 import DropDownMenus from "./components/DropDownMenus";
 
 // Material Dashboard 2 React components
@@ -25,7 +25,7 @@ import DashboardLayout from "../../components/LayoutContainers/DashboardLayout";
 import Footer from "../../components/Footer";
 import ReportsLineChart from "../../components/Charts/LineCharts/ReportsLineChart";
 import ComplexStatisticsCard from "../../components/Cards/StatisticsCards/ComplexStatisticsCard";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+// import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 // Data
 import reportsLineChartData from "./data/reportsLineChartData";
@@ -35,7 +35,7 @@ import { API, Auth, PubSub } from "aws-amplify";
 import dayjs from "dayjs";
 import dayOfYear from "dayjs/plugin/dayOfYear.js";
 import toObject from "dayjs/plugin/toObject.js";
-import { ListItemIcon } from "@mui/material";
+// import { ListItemIcon } from "@mui/material";
 dayjs.extend(dayOfYear);
 dayjs.extend(toObject);
 
@@ -270,7 +270,7 @@ const DashboardContainer = ({ iotThingNames, unitOfMass, displayIngredientIndex 
 
     return (
         <DashboardLayout>
-            <div style={{ margin: "auto ", marginTop: "15px", width: "fit-content", border: "1px solid #49a3f1 ", borderRadius: "5px", padding: "5px", marginLeft: "0px" }}>
+            {/* <div style={{ margin: "auto ", marginTop: "15px", width: "fit-content", border: "1px solid #49a3f1 ", borderRadius: "5px", padding: "5px", marginLeft: "0px" }}>
                 <List component="nav" aria-label="Device settings">
                     <ListItem
                         id="lock-button"
@@ -308,8 +308,15 @@ const DashboardContainer = ({ iotThingNames, unitOfMass, displayIngredientIndex 
                         </MenuItem>
                     ))}
                 </Menu>
-            </div>
-            <DropDownMenus options={options} selectedIndexRef={selectedIndexRef} />
+            </div> */}
+            <DropDownMenus
+                options={options}
+                selectedIndexRef={selectedIndexRef}
+                selectedIndex={selectedIndex}
+                handleClickListItem={handleClickListItem}
+                handleMenuItemClick={handleMenuItemClick}
+                handleClose={handleClose}
+            />
             <MDBox py={3}>
                 <Grid container spacing={3}>
                     <Grid item xs={12} md={6} lg={3}>
