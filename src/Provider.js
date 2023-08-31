bc79f4292c6c4568eeead9c946c849170cd7894e
atellezcastr <atellezcastr@mun.ca>
2023-08-31 20:37:10 -0230

    fix: bug causing infinite loop

--- src/Provider.js
+++ src/Provider.js
@@ -3,19 +3,19 @@ import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
 import defaultStore from "./redux/store";
-import theme from "./assets/theme";
-import App from "./App";
+// import App from "./App";
 
 const ProviderWrapper = ({
     children,
     darkMode,
+    theme,
     store = defaultStore
 }) => {
     return (
         <Provider store={store}>
             <ThemeProvider theme={darkMode? null: theme}>
                 <CssBaseline />
-                <App />
+                {children}
             </ThemeProvider>
         </Provider>
     )
