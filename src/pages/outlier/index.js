5a26b1533b7ea1e2b3cebd4ded357d42aeccf87e
jarco0204 <jaarcos@mun.ca>
2023-09-01 14:12:17 -0230

    add: Outlier Detection Graph

--- src/pages/outlier/index.js
+++ src/pages/outlier/index.js
@@ -1,9 +1,34 @@
 // Main Libraries
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import Chart from "chart.js/auto";
+
+// MUI Imports
+import Grid from "@mui/material/Grid";
+
+// User Components
+import DashboardLayout from "../../components/LayoutContainers/DashboardLayout";
+import Footer from "../../components/Footer";
+import MDBox from "../../components/MDBox";
+import PortionWeightLineChart from "./components/PortionWeightLineChart";
 
 // Backend
-import { API, Auth } from "aws-amplify";
+import { PubSub } from "aws-amplify";
+
+/*!
+   @description: Helper function ton create an object to store the portion event data.
+   @params:
+   @return:
+   @Comments
+   @Coders: Wat€r1110
+*/
+const createReportLineChartObject = () => {
+    return {
+        labels: [],
+        datasets: { label: "Portion Accuracy", data: [], yAxisLabel: "Percent" },
+        pointBackgroundColorAr: [],
+    };
+};
 
 /*!
    @description:
@@ -13,9 +38,47 @@ import { API, Auth } from "aws-amplify";
    @Coders: Fu€g0001
 */
 const OutlierContainer = ({ iotThingNames, displayIngredient }) => {
+    // State of Container
+    const [realTimePortionEventsAR, setRealTimePortionEventsAR] = useState([]); //
+    const [realTimePortionEventChart, setRealTimePortionEventChart] = useState([]);
+    const { weightGraph } = createReportLineChartObject();
+
+    //Use Effects
+    useEffect(() => {
+        PubSub.subscribe("test/rohan/1/od").subscribe({
+            next: (data) => {
+                // console.log("Message received", data.value);
+                console.log("timestamp is", data.value.timestamp);
+                console.log("portion weight is", data.value.portionWeight);
+                console.log("portion status is", data.value.portionStatus);
+                console.log();
+                setRealTimePortionEventChart(weightGraph);
+            },
+            error: (error) => console.error(error),
+            complete: () => console.log("Done"),
+        });
+    }, []);
+
     // Display Outlier Page
-    return <h1>FUEGO</h1>;
+    return (
+        <DashboardLayout>
+            <MDBox py={1}>
+                <MDBox mt={10}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={10} lg={12}>
+                            <MDBox mb={40}>
+                                <PortionWeightLineChart color="success" title="Portion Accuracy Classification" chart={realTimePortionEventChart} />
+                            </MDBox>
+                        </Grid>
+                    </Grid>
+                </MDBox>
+            </MDBox>
+            <Footer />
+        </DashboardLayout>
+    );
 };
+
+// Handle the props
 OutlierContainer.propTypes = {
     iotThingNames: PropTypes.array,
     displayIngredient: PropTypes.string,
