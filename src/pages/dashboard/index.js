c865149ace9854e0f54b300c7f851e26c0a1d335
atellezcastr <atellezcastr@mun.ca>
2023-09-04 17:01:37 -0230

    feat: added redux to the dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -131,15 +131,12 @@ const DashboardContainer = () => {
     const displayIngredientIndex = useSelector(state => state.meta.displayIngredient)
     const timeZone = useSelector(state => state.meta.timeZone)
     const demo = useSelector(state => state.meta.demo)
-<<<<<<< HEAD
     const portionCompleteTitle = "Portions Completed";
     const portionPrecisionTitle = "Precision Levels";
     const portionTimeTitle = "Average Completion Time";
     const inventoryConsumedTitle = "Inventory Consumed";
     const [isMobileDevice, setIsMobileDevice] = useState(clientDemo);
 
-=======
->>>>>>> b8853f5 (feat: added redux to the dashboard)
     const [cardSummaryItems, setCardSummaryItems] = useState([]);
     const [realTimePrecisionGraph, setRealTimePrecisionGraph] = useState([]);
     const [realTimeAccuracyGraph, setRealTimeAccuracyGraph] = useState([]);
@@ -281,12 +278,7 @@ const DashboardContainer = () => {
     const getHourlyMetaRecords = async () => {
         try {
             let tempDate = dayjs().tz(timeZone); // Local time of Client
-<<<<<<< HEAD
-
             // Query GQL to pull hourly data
-=======
-            
->>>>>>> b8853f5 (feat: added redux to the dashboard)
             const response = await API.graphql({
                 query: getDay,
                 variables: { dayOfYear_iotNameThing: tempDate.dayOfYear().toString() + "_" + keys[selectedIndexRef.current] }, // Provide the ID as a variable
@@ -351,7 +343,6 @@ const DashboardContainer = () => {
         };
     }, [selectedIndex]);
 
-<<<<<<< HEAD
     // UseEffect to change layout for mobile devices
     useEffect(() => {
         const handleResize = () => {
@@ -368,8 +359,6 @@ const DashboardContainer = () => {
 
   
 
-=======
->>>>>>> b8853f5 (feat: added redux to the dashboard)
     const convertGsToOz = (val) => {
         return (parseInt(val) / 28.35).toFixed(2).toString()
     }
@@ -401,15 +390,12 @@ const DashboardContainer = () => {
                                 <MDBox mb={1.5}>
                                     <ComplexStatisticsCard
                                         color="info"
-<<<<<<< HEAD
                                         icon={<PrecisionManufacturingRoundedIcon />}
                                         title={portionPrecisionTitle}
                                         count={cardSummaryItems[1]}
-=======
                                         icon={<ScaleRoundedIcon />}
                                         title="Total Consumed Inventory"
                                         count={unitOfMass == "g" ? cardSummaryItems[2] : convertGsToOz(cardSummaryItems[2]) + "oz"}
->>>>>>> b8853f5 (feat: added redux to the dashboard)
                                         percentage={{
                                             color: "success",
                                             // amount: "+3%",
@@ -479,15 +465,12 @@ const DashboardContainer = () => {
                             <Grid item xs={12} md={6} lg={3}>
                                 <MobileComplexStatisticsCard
                                     color="info"
-<<<<<<< HEAD
                                     icon={<PrecisionManufacturingRoundedIcon />}
                                     title={portionPrecisionTitle}
                                     count={cardSummaryItems[1]}
-=======
                                     icon={<ScaleRoundedIcon />}
                                     title="Total Consumed Inventory"
                                     count={unitOfMass == "g" ? cardSummaryItems[2] : convertGsToOz(cardSummaryItems[2]) + "oz"}
->>>>>>> b8853f5 (feat: added redux to the dashboard)
                                     percentage={{
                                         color: "success",
                                     }}
